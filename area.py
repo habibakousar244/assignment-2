d46378a080c0ae73eb7e377b1d0c0952855f4753
habibakousar244 <habibarazzaq244@gmail.com>
2024-06-12 16:41:03 +0500

    Add files via upload

---
+++ area.py
@@ -0,0 +1,18 @@
+#triangle
+base = 10
+height = 7
+form = 0.5
+area_of_triangle = form*base*height 
+print(area_of_triangle)
+
+#square
+side1 = 8
+side2 = 8
+area_of_square = side1*side2
+print(area_of_square)
+
+#circle
+circle_form = 3.14
+radius = 8
+area_of_circle = circle_form*radius*radius
+print(area_of_circle)  
